2d19228d782bcd26a8fdd87f9c215dd2c4945744
saulperez37 <sperez_44@hotmail.com>
2019-11-14 23:45:00 -0700

    fixed routes, added html and bootstrap

--- server.js
+++ server.js
@@ -4,43 +4,13 @@ const friends = require("./data/friends");
 
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-app.get("/api/friends", function (req, res) {
-    res.json(friends);
-});
-
-app.get("/api/friends/:name", function (req,res) {
-    let friendName = req.params.name;
-
-    for (let i = 0; i < friends.length; i++) {
-        if (friends[i].name.toLowerCase() === friendName) {
-            let friend = friends[i];
-            console.log(`Friend found: ${friend}`);
-            
-            return res.json(friend);
-        }
-    };
-
-    return res.send(false);
-});
-
-app.post("/api/friends", function (req, res) {
-   const newFriend = req.body;
-   
-   newFriend.name = (req.body.name.toLowerCase());
-
-   console.log(`New friend info: ${newFriend}`);
-   
-   friends.push(newFriend);
-
-   res.json(newFriend);
-});
-
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
 app.listen(PORT, function () {
