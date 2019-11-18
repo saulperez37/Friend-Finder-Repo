6e7f913325d329e2359dff77a8e7ff0bc3ba17f2
saulperez37 <sperez_44@hotmail.com>
2019-11-17 22:47:21 -0700

    adding link to git hub

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -21,15 +21,43 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        const newFriend = req.body;
-    
-        newFriend.name = (req.body.name.toLowerCase());
-    
-        console.log(`New friend info: ${newFriend}`);
-    
+        const newFriend = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: []
+        };
+
+        const scoresArr = [];
+        for (let i = 0; i < req.body.scores.length; i++) {
+            scoresArr.push(parseInt(req.body.scores[i])
+            )
+        }
+
+        newFriend.scores = scoresArr;
+
+        const compareScoreArr = [];
+        for (let j = 0; j < friends.length; j++) {
+
+            const comparison = 0;
+            for (let k = 0; k < newFriend.scores.length; k++) {
+                comparison += Math.abs(newFriend.scores[k] - friends[k].scores[k]);
+            }
+            compareScoreArr.push(comparison);
+        }
+
+        const topMatch = 0;
+        for (let l = 1; l < compareScoreArr.length; l++) {
+
+            if (compareScoreArr[l] <= compareScoreArr[topMatch]) {
+                topMatch = l;
+            };
+        };
+
+        const topFriendMatch = friends[topMatch];
+
+        res.json(topFriendMatch);
+
         friends.push(newFriend);
-    
-        res.json(newFriend);
     });
 };
 
